570d3405bc9ccad14eba23976e85d4097587878e
iftekhar1405 <iftekharahmedxyz@gmail.com>
2024-10-01 16:30:14 +0530

    made it single paged

--- src/components/Home/Home2.js
+++ src/components/Home/Home2.js
@@ -1,101 +1,140 @@
-import React from "react";
+import React, { useRef } from "react";
 import { Container, Row, Col } from "react-bootstrap";
 import myImg from "../../Assets/myimg.jpg";
 import Tilt from "react-parallax-tilt";
-import {
-  AiFillGithub,
-  AiFillInstagram,
-} from "react-icons/ai";
+import { AiFillGithub, AiFillInstagram } from "react-icons/ai";
 import { FaLinkedinIn } from "react-icons/fa";
+import About from "../About/About";
+import Projects from "../Projects/Projects";
+import ResumeNew from "../Resume/ResumeNew";
+import NavBar from "../Navbar";  // Import NavBar to pass scroll props
 
 function Home2() {
+  // Create refs for the sections
+  const aboutRef = useRef(null);
+  const projectsRef = useRef(null);
+  const resumeRef = useRef(null);
+
+  // Scroll function to scroll to the section
+  const scrollToSection = (ref) => {
+    ref.current.scrollIntoView({ behavior: "smooth" });
+  };
+
   return (
-    <Container fluid className="home-about-section" id="about">
-      <Container>
-        <Row>
-          <Col md={8} className="home-about-description">
-            <h1 style={{ fontSize: "2.6em" }}>
-              LET ME <span className="purple"> INTRODUCE </span> MYSELF
-            </h1>
-            <p className="home-about-body">
-              I fell in love with programming and I have at least learnt
-              something, I think… 🤷‍♂️
-              <br />
-              <br />I am fluent in classics like
-              <i>
-                <b className="purple">Python, Javascript, React, MongoDB, SQL, Node, Express, DSA, Prompt Eng. </b>
-              </i>
-              <br />
-              <br />
-              My field of Interest's are building new &nbsp;
-              <i>
-                <b className="purple">Web Technologies and Products </b>
-              </i>
-              <br />
-              <br />
-              Whenever possible, I also apply my passion for developing products
-              with <b className="purple">Node.js</b> and
-              <i>
-                <b className="purple">
-                  {" "}
-                  Modern Javascript Library and Frameworks
-                </b>
-              </i>
-              &nbsp; like
-              <i>
-                <b className="purple"> React.js and Express.js</b>
-              </i>
-            </p>
-          </Col>
-          <Col md={4} className="myAvtar">
-            <Tilt>
-              <img src={myImg} className="img-fluid" alt="avatar" style={{verticalAlign:'middle', borderRadius:'700px'}} />
-            </Tilt>
-          </Col>
-        </Row>
-        <Row>
-          <Col md={12} className="home-about-social">
-            <h1>FIND ME ON</h1>
-            <p>
-              Feel free to <span className="purple">connect </span>with me
-            </p>
-            <ul className="home-about-social-links">
-              <li className="social-icons">
-                <a
-                  href="https://github.com/Iftekhar1405"
-                  target="_blank"
-                  rel="noreferrer"
-                  className="icon-colour  home-social-icons"
-                >
-                  <AiFillGithub />
-                </a>
-              </li>
-              
-              <li className="social-icons">
-                <a
-                  href="https://www.linkedin.com/in/iftekhar-ahmed-857606188/"
-                  target="_blank"
-                  rel="noreferrer"
-                  className="icon-colour  home-social-icons"
-                >
-                  <FaLinkedinIn />
-                </a>
-              </li>
-              <li className="social-icons">
-                <a
-                  href="https://www.instagram.com/iftekharahmedx/?next=%2F&hl=en"
-                  target="_blank"
-                  rel="noreferrer"
-                  className="icon-colour home-social-icons"
-                >
-                  <AiFillInstagram />
-                </a>
-              </li>
-            </ul>
-          </Col>
-        </Row>
+    <>
+      {/* Pass the scroll function and refs to NavBar */}
+      <NavBar
+        scrollToSection={scrollToSection}
+        aboutRef={aboutRef}
+        projectsRef={projectsRef}
+        resumeRef={resumeRef}
+      />
+
+      <Container fluid className="home-about-section" id="about">
+        <Container>
+          <Row>
+            <Col md={8} className="home-about-description">
+              <h1 style={{ fontSize: "2.6em" }}>
+                LET ME <span className="purple"> INTRODUCE </span> MYSELF
+              </h1>
+              <p className="home-about-body">
+                I fell in love with programming and I have at least learnt something, I think… 🤷‍♂️
+                <br />
+                <br />I am fluent in classics like
+                <i>
+                  <b className="purple">
+                    Python, Javascript, React, MongoDB, SQL, Node, Express, DSA, Prompt Eng.
+                  </b>
+                </i>
+                <br />
+                <br />
+                My field of Interest's are building new &nbsp;
+                <i>
+                  <b className="purple">Web Technologies and Products</b>
+                </i>
+                <br />
+                <br />
+                Whenever possible, I also apply my passion for developing products with{" "}
+                <b className="purple">Node.js</b> and
+                <i>
+                  <b className="purple">
+                    {" "}
+                    Modern Javascript Library and Frameworks
+                  </b>
+                </i>{" "}
+                &nbsp; like
+                <i>
+                  <b className="purple"> React.js and Express.js</b>
+                </i>
+              </p>
+            </Col>
+            <Col md={4} className="myAvtar">
+              <Tilt>
+                <img
+                  src={myImg}
+                  className="img-fluid"
+                  alt="avatar"
+                  style={{ verticalAlign: "middle", borderRadius: "700px" }}
+                />
+              </Tilt>
+            </Col>
+          </Row>
+          <Row>
+            <Col md={12} className="home-about-social">
+              <h1>FIND ME ON</h1>
+              <p>
+                Feel free to <span className="purple">connect </span>with me
+              </p>
+              <ul className="home-about-social-links">
+                <li className="social-icons">
+                  <a
+                    href="https://github.com/Iftekhar1405"
+                    target="_blank"
+                    rel="noreferrer"
+                    className="icon-colour  home-social-icons"
+                  >
+                    <AiFillGithub />
+                  </a>
+                </li>
+
+                <li className="social-icons">
+                  <a
+                    href="https://www.linkedin.com/in/iftekhar-ahmed-857606188/"
+                    target="_blank"
+                    rel="noreferrer"
+                    className="icon-colour  home-social-icons"
+                  >
+                    <FaLinkedinIn />
+                  </a>
+                </li>
+                <li className="social-icons">
+                  <a
+                    href="https://www.instagram.com/iftekharahmedx/?next=%2F&hl=en"
+                    target="_blank"
+                    rel="noreferrer"
+                    className="icon-colour home-social-icons"
+                  >
+                    <AiFillInstagram />
+                  </a>
+                </li>
+              </ul>
+            </Col>
+          </Row>
+        </Container>
       </Container>
-    </Container>
+
+      {/* Adding ref to sections */}
+      <div ref={aboutRef}>
+        <About />
+      </div>
+      <div ref={projectsRef}>
+        <Projects />
+      </div>
+      <div ref={resumeRef}>
+        <ResumeNew />
+      </div>
+    </>
   );
 }
+
 export default Home2;
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,7 +3,6 @@ import Navbar from "react-bootstrap/Navbar";
 import Nav from "react-bootstrap/Nav";
 import Container from "react-bootstrap/Container";
 import Button from "react-bootstrap/Button";
-import { Link } from "react-router-dom";
 import { CgGitFork } from "react-icons/cg";
 import {
   AiFillStar,
@@ -11,10 +10,9 @@ import {
   AiOutlineFundProjectionScreen,
   AiOutlineUser,
 } from "react-icons/ai";
-
 import { CgFileDocument } from "react-icons/cg";
 
-function NavBar() {
+function NavBar({ scrollToSection, aboutRef, projectsRef, resumeRef }) {
   const [expand, updateExpanded] = useState(false);
   const [navColour, updateNavbar] = useState(false);
 
@@ -37,7 +35,7 @@ function NavBar() {
     >
       <Container>
         <Navbar.Brand href="/" className="d-flex">
-        <strong style={{color:'#cd5ff8'}}>Iftekhar Ahmed</strong>
+          <strong style={{ color: '#cd5ff8' }}>Iftekhar Ahmed</strong>
         </Navbar.Brand>
         <Navbar.Toggle
           aria-controls="responsive-navbar-nav"
@@ -52,16 +50,12 @@ function NavBar() {
         <Navbar.Collapse id="responsive-navbar-nav">
           <Nav className="ms-auto" defaultActiveKey="#home">
             <Nav.Item>
-              <Nav.Link as={Link} to="/" onClick={() => updateExpanded(false)}>
-                <AiOutlineHome style={{ marginBottom: "2px" }} /> Home
-              </Nav.Link>
-            </Nav.Item>
-
-            <Nav.Item>
               <Nav.Link
-                as={Link}
-                to="/about"
-                onClick={() => updateExpanded(false)}
+                href="#"
+                onClick={() => {
+                  scrollToSection(aboutRef);
+                  updateExpanded(false);
+                }}
               >
                 <AiOutlineUser style={{ marginBottom: "2px" }} /> About
               </Nav.Link>
@@ -69,27 +63,27 @@ function NavBar() {
 
             <Nav.Item>
               <Nav.Link
-                as={Link}
-                to="/project"
-                onClick={() => updateExpanded(false)}
+                href="#"
+                onClick={() => {
+                  scrollToSection(projectsRef);
+                  updateExpanded(false);
+                }}
               >
-                <AiOutlineFundProjectionScreen
-                  style={{ marginBottom: "2px" }}
-                />{" "}
-                Projects
+                <AiOutlineFundProjectionScreen style={{ marginBottom: "2px" }} /> Projects
               </Nav.Link>
             </Nav.Item>
 
             <Nav.Item>
               <Nav.Link
-                as={Link}
-                to="/resume"
-                onClick={() => updateExpanded(false)}
+                href="#"
+                onClick={() => {
+                  scrollToSection(resumeRef);
+                  updateExpanded(false);
+                }}
               >
                 <CgFileDocument style={{ marginBottom: "2px" }} /> Resume
               </Nav.Link>
             </Nav.Item>
-
 
             <Nav.Item className="fork-btn">
               <Button
