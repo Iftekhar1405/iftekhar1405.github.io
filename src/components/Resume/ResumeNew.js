caf5bd6658dd4ad132457602806edfa99f9f1d5b
iftekhar1405 <iftekharahmedxyz@gmail.com>
2024-10-01 16:48:04 +0530

    whatsapp included

--- src/components/Resume/ResumeNew.js
+++ src/components/Resume/ResumeNew.js
@@ -1,11 +1,12 @@
 import React, { useState, useEffect } from "react";
-import { Container, Row } from "react-bootstrap";
+import { Col, Container, Row } from "react-bootstrap";
 import Button from "react-bootstrap/Button";
 import Particle from "../Particle";
 import pdf from "../../Assets/../Assets/IftekharAhmad-FullStackWebDeveloper-3F10.pdf";
-import { AiOutlineDownload } from "react-icons/ai";
+import { AiFillGithub, AiFillInstagram, AiOutlineDownload } from "react-icons/ai";
 import { Document, Page, pdfjs } from "react-pdf";
 import "react-pdf/dist/esm/Page/AnnotationLayer.css";
+import {  FaLinkedinIn, FaWhatsapp } from "react-icons/fa";
 pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.min.js`;
 
 function ResumeNew() {
@@ -47,6 +48,57 @@ function ResumeNew() {
             <AiOutlineDownload />
             &nbsp;Download CV
           </Button>
+          <Row>
+          <Col md={12} className="home-about-social">
+              <h1>FIND ME ON</h1>
+              <p>
+                Feel free to <span className="purple">connect </span>with me
+              </p>
+              <ul className="home-about-social-links">
+              <li className="social-icons">
+                  <a
+                    href="https://wa.me/9977079639?text=Hello%20I%20came%20across%20your%20portfolio%20and%20would%20like%20to%20connect"
+                    target="_blank"
+                    rel="noreferrer"
+                    className="icon-colour  home-social-icons"
+                  >
+                    <FaWhatsapp />
+                  </a>
+                </li>
+                <li className="social-icons">
+                  <a
+                    href="https://github.com/Iftekhar1405"
+                    target="_blank"
+                    rel="noreferrer"
+                    className="icon-colour  home-social-icons"
+                  >
+                    <AiFillGithub />
+                  </a>
+                </li>
+
+                <li className="social-icons">
+                  <a
+                    href="https://www.linkedin.com/in/iftekhar-ahmed-857606188/"
+                    target="_blank"
+                    rel="noreferrer"
+                    className="icon-colour  home-social-icons"
+                  >
+                    <FaLinkedinIn />
+                  </a>
+                </li>
+                <li className="social-icons">
+                  <a
+                    href="https://www.instagram.com/iftekharahmedx/?next=%2F&hl=en"
+                    target="_blank"
+                    rel="noreferrer"
+                    className="icon-colour home-social-icons"
+                  >
+                    <AiFillInstagram />
+                  </a>
+                </li>
+              </ul>
+            </Col>
+            </Row>
         </Row>
       </Container>
     </div>
